1804c8d0eb46b81d159d001d92e80c80ffcabb4f
Moises26b <mbenavides10786@ufide.ac.cr>
2023-10-01 14:42:29 -0600

    Add files via upload

---
+++ Tarea2/src/main/java/com/Tarea2/Tiendav1Application.java
@@ -0,0 +1,13 @@
+package com.Tarea2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tiendav1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tiendav1Application.class, args);
+	}
+
+}
